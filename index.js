b4eafafc5acfa1f50925a935561a2965fdbe3ec5
Muhammad Ahmed <muhammadahmed120192@gmail.com>
2023-11-02 20:11:51 +0500

    modified

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 import express from 'express'
 import router from './routes/index.js'
-import mongoose from 'mongoose'
-import databaseConnection from './db/index.js'
-import chalk from 'chalk'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import databaseConnection from './db/db.js'
+import chalk from 'chalk'
 
 const PORT = 3001
+
+// DATABASE CONNECTION
+
+const database = mongoose.connection;
+database.on("error", console.error.bind(console, chalk.bgRedBright.whiteBright("connection error:")));
+database.once("open", function () {
+    console.log(chalk.bgYellowBright.grey("db connected!"));
+});
 
 const app = express()
 app.use(express.json())
 app.use(cors())
-
-// DATABASE CONNECTION
-databaseConnection('Orders')
-
-const database = mongoose.connection;
-database.on("error", console.error.bind(console, "connection error:"));
-database.once("open", function () {
-  console.log(chalk.bgYellowBright.grey("db connected!"));
-});
-
 
 app.use("/", router)
 
