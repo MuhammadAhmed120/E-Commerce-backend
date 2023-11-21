d226bb3b6ff8530ea56fd075588c6020f27e70f3
Muhammad Ahmed <muhammadahmed120192@gmail.com>
2023-11-22 03:33:25 +0500

    Update verifyToken.js

--- token/verifyToken.js
+++ token/verifyToken.js
@@ -1,13 +1,16 @@
 import jwt from "jsonwebtoken"
+import 'dotenv/config'
 
 const verifyToken = (req, res, next) => {
     try {
         const { authorization } = req.headers;
         const token = authorization && authorization.split(" ")[1];
-        jwt.verify(token, 'SMIT', function (err, decoded) {
+        jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
             if (err) {
-                return res.status(401).send({ message: "unauthorized", err })
+                return res.status(401).send({ message: "unauthorized token", err })
             }
+            req.decodedToken = decoded;
+
             return next()
         });
     } catch (err) {
