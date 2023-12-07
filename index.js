6bf0f4646a86d11ddffcb4154ffaf99be2bd4ffa
Muhammad Ahmed <muhammadahmed120192@gmail.com>
2023-12-08 03:28:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 import express from 'express'
 import router from './routes/index.js'
 import cors from 'cors'
-import mongoose from 'mongoose'
-import chalk from 'chalk'
+// import mongoose from 'mongoose'
+// import chalk from 'chalk'
+import connectDB from './db/db.js'
 import 'dotenv/config'
 
 const PORT = process.env.PORT || 3002
 
 // DATABASE CONNECTION
-const database = mongoose.connection;
-database.on("error", console.error.bind(console, chalk.bgRedBright.whiteBright("connection error:")));
-database.once("open", function () {
-    console.log(chalk.bgYellowBright.grey("db connected!"));
-});
+// const database = mongoose.connection;
+// database.on("error", console.error.bind(console, chalk.bgRedBright.whiteBright("connection error:")));
+// database.once("open", function () {
+//     console.log(chalk.bgYellowBright.grey("db connected!"));
+// });
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 app.use("/images", express.static('images'))
-
 app.use("/", router)
 
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}.`)
-})
+    (async () => {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server running on ${PORT}.`);
+        });
+    })();
