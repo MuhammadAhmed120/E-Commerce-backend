bf3100b2faa5432adc40f61baedbfc04c40319cc
Muhammad Ahmed <muhammadahmed120192@gmail.com>
2023-12-08 03:28:21 +0500

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,24 +1,26 @@
 import mongoose from "mongoose";
-import 'dotenv/config'
+import 'dotenv/config';
 
-const USER_URI = process.env.USER_URI_DB
-const ORDER_URI = process.env.ORDER_URI_DB
+const USER_URI = process.env.USER_URI_DB;
+const ORDER_URI = process.env.ORDER_URI_DB;
 
-const connectDB = () => {
+const connectDB = async () => {
     try {
-        const userDB = mongoose.createConnection(USER_URI, {
+        const userDB = await mongoose.createConnection(USER_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true
-        })
-        const orderDB = mongoose.createConnection(ORDER_URI, {
+        });
+
+        const orderDB = await mongoose.createConnection(ORDER_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true
-        })
-        return { userDB, orderDB }
+        });
+
+        return { userDB, orderDB };
     } catch (error) {
-        console.error(`Error:${error.message}`)
-        process.exit(1)
+        console.error(`Error:${error.message}`);
+        process.exit(1);
     }
-}
+};
 
 export default connectDB;
